eeb8cc8f2af9025db642b9c3757cd3e1f274b3ff
seaman <semen.bko@ya.ru>
2014-12-22 10:06:14 +0300

    lesson1

---
+++ less1.js
@@ -0,0 +1,23 @@
+// To get into the rhythm, let's do a simple hello world.
+
+// Write a Node program that accepts two command-line arguments, X and
+// Y and prints out to the console the following text:
+
+//   ALL YOUR X ARE BELONG TO Y
+
+// When you have completed your program, you can run it in the test
+// environment with:
+
+//   levelmeup run program.js
+
+// And once you are happy that it is correct then run:
+
+//   levelmeup verify program.js
+
+// And your submission will be verified for correctness. After you have
+// a correct solution, run `levelmeup` again and select the next problem!
+
+var X = process.argv[2],
+	Y = process.argv[3];
+
+console.log('ALL YOUR ' + X + ' ARE BELONG TO ' + Y);
